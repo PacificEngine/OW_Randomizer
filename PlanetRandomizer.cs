4f56c6ef5b73d35d8e3517c1e13aa359ba2e1a6a
JoJo <joseph.salomone@dealertrack.com>
2021-12-14 22:34:17 -0800

    Something

--- PlanetRandomizer.cs
+++ PlanetRandomizer.cs
@@ -62,15 +62,14 @@ namespace PacificEngine.OW_Randomizer
                 case Position.HeavenlyBodies.HourglassTwins:
                 case Position.HeavenlyBodies.Attlerock:
                     // These object's rotation is set based on game state
-                    return new Planet.Plantoid(original.size, original.gravity, original.state?.orbit?.rotation ?? original.state.relative.rotation, original.state?.orbit?.angularVelocity.magnitude ?? original.state.relative.angularVelocity.magnitude, original.state.parent, randomKepler(parent, original));
+                    return new Planet.Plantoid(original.size, original.gravity, Quaternion.identity, 0f, original.state.parent, randomKepler(parent, original));
                 case Position.HeavenlyBodies.TimberHearth:
                 case Position.HeavenlyBodies.TimberHearthProbe:
                 case Position.HeavenlyBodies.DarkBramble:
                 case Position.HeavenlyBodies.Interloper:
                 case Position.HeavenlyBodies.BackerSatilite:
                 case Position.HeavenlyBodies.HollowLantern:
-                    return new Planet.Plantoid(original.size, original.gravity, original.state?.orbit?.rotation ?? original.state.relative.rotation, original.state?.orbit?.angularVelocity.magnitude ?? original.state.relative.angularVelocity.magnitude, original.state.parent, randomKepler(parent, original));
-                    //return new Planet.Plantoid(original.size, original.gravity, randomQuaternion(), (float)seeds.NextRange(-0.2, 0.2), original.state.parent, randomKepler(parent, original));
+                    return new Planet.Plantoid(original.size, original.gravity, randomQuaternion(), (float)seeds.NextRange(-0.2, 0.2), original.state.parent, randomKepler(parent, original));
                 case Position.HeavenlyBodies.WhiteHole:
                     // White Hole do not obey gravity
                     return new Planet.Plantoid(original.size, original.gravity, randomQuaternion(), original.state.relative.angularVelocity.magnitude, original.state.parent, randomPosition(parent, original), original.state.relative.velocity);
