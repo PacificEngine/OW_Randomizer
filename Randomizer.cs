77bd32eb684f8c3260b2c6537c82047f7e48cb2a
JoJo <joseph.salomone@dealertrack.com>
2022-01-01 23:11:52 -0800

    Upped Version

--- Randomizer.cs
+++ Randomizer.cs
@@ -15,7 +15,7 @@ namespace PacificEngine.OW_Randomizer
 {
     public class MainClass : ModBehaviour
     {
-        private const string verison = "0.7.4";
+        private const string verison = "0.7.5";
 
         /*  TH_ZERO_G_CAVE_R1(TH_ZERO_G_CAVE) 
          *  SCIENTIST_1(True)
